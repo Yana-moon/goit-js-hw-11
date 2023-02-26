4be51a583cb9cf46cc8a73437cdff44a9ecac170
KhailoAndrey <110947394+KhailoAndrey@users.noreply.github.com>
2023-02-26 12:09:44 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import axios from 'axios';
 
 
 const userId = '33912044-22b2651672bec86fc9e274e80';
-const requestUrl = 'https://pixabay.com/api';
+const requestUrl = 'https://pixabay.com/api/?';
 
 const emptyRequest =
   'Sorry, there are no images matching your search query. Please try again.';
